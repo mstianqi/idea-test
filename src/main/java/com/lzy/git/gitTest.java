720d2f1d139c2705ce18277bc1c64f0eb56a4b1f
Tianqi <Tian@qi.com>
2023-12-21 09:50:21 +0800

    ssh commit

--- src/main/java/com/lzy/git/gitTest.java
+++ src/main/java/com/lzy/git/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("master");
         System.out.println("hot");
         System.out.println("push test");
+        System.out.println("ssh test");
     }
 }
